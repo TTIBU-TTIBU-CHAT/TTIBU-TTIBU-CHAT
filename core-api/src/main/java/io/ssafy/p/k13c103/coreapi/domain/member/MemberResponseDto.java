a16d8a2e43757269636c266db89eedb968f12cbe
ahyolo27 <ahhyeon27@gmail.com>
2025-11-01 00:23:46 +0900

    feat: 회원가입 DTO 생성

---
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/member/MemberResponseDto.java
@@ -0,0 +1,15 @@
+package io.ssafy.p.k13c103.coreapi.domain.member;
+
+import lombok.Builder;
+
+public class MemberResponseDto {
+
+    /**
+     * 회원가입 응답 DTO
+     */
+    @Builder
+    public record RegisteredMemberInfo(
+            Long memberUid
+    ) {
+    }
+}
