7c7ad8a8576361f00dcbc50c1bdb47dc0c0c8c5e
IM-GYURI <kgulr0517@gmail.com>
2025-11-02 18:02:21 +0900

    fix(branch): JpaRepository 제네릭 타입을 Room → Branch로 수정

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/branch/repository/BranchRepository.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/branch/repository/BranchRepository.java
@@ -1,8 +1,8 @@
 package io.ssafy.p.k13c103.coreapi.branch.repository;
 
-import io.ssafy.p.k13c103.coreapi.room.entity.Room;
+import io.ssafy.p.k13c103.coreapi.branch.entity.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BranchRepository extends JpaRepository<Room, Long> {
+public interface BranchRepository extends JpaRepository<Branch, Long> {
 
 }
