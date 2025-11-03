2d770ff7b2875a2bc24755f5f75f8c950410abd9
IM-GYURI <kgulr0517@gmail.com>
2025-11-03 16:33:09 +0900

    feat(chat): ROOM_CREATED 이벤트 타입 추가

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/chat/enums/ChatSseEventType.java
+++
@@ -1,8 +0,0 @@
-package io.ssafy.p.k13c103.coreapi.chat.enums;
-
-public enum ChatSseEventType {
-
-    CHAT_CREATED,
-    CHAT_ANSWERED,
-    CHAT_UPDATED
-}
---
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/chat/enums/ChatSseEventType.java
@@ -0,0 +1,10 @@
+package io.ssafy.p.k13c103.coreapi.domain.chat.enums;
+
+public enum ChatSseEventType {
+
+    ROOM_CREATED,
+    CHAT_CREATED,
+    CHAT_ANSWERED,
+    CHAT_SUMMARIZED,
+    CHAT_UPDATED
+}
