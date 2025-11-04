c30ad8124b292a3edacce89d75e585b729bd2b5a
ahyolo27 <ahhyeon27@gmail.com>
2025-11-04 11:40:40 +0900

    feat: 키 단일 조회 DTO 생성

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/key/dto/KeyResponseDto.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/key/dto/KeyResponseDto.java
@@ -2,6 +2,8 @@ package io.ssafy.p.k13c103.coreapi.domain.key.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 public class KeyResponseDto {
 
     /**
@@ -19,6 +21,20 @@ public class KeyResponseDto {
     @Builder
     public record EditKeyInfo(
             Long keyUid
+    ) {
+    }
+
+    /**
+     * 키 조회 응답 DTO
+     */
+    @Builder
+    public record GetKeyInfo(
+            Long keyUid,
+            String provider,
+            String key, // 복호화한 키
+            Boolean isActive,
+            LocalDate expirationAt
+
     ) {
     }
 }
