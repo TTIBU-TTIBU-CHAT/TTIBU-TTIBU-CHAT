67c045f7d76bd9014d58251977ae5e9eafe261f1
ahyolo27 <ahhyeon27@gmail.com>
2025-11-05 09:42:44 +0900

    feat: 사용자가 선택한 모델을 포함한 전체 이용 가능 모델 리스트 반환 로직 구현

---
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/model/repository/ModelRepository.java
@@ -0,0 +1,10 @@
+package io.ssafy.p.k13c103.coreapi.domain.model.repository;
+
+import io.ssafy.p.k13c103.coreapi.domain.model.entity.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ModelRepository extends JpaRepository<Model, Long> {
+    List<Model> findAllByMember_MemberUidAndProviderAndCodeIn(Long memberUid, String provider, List<String> codes);
+}
---
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/model/service/ModelService.java
@@ -0,0 +1,10 @@
+package io.ssafy.p.k13c103.coreapi.domain.model.service;
+
+import io.ssafy.p.k13c103.coreapi.domain.model.dto.ModelResponseDto;
+
+import java.util.List;
+
+public interface ModelService {
+
+    List<ModelResponseDto.ModelListInfo> getModels(Long memberUid);
+}
