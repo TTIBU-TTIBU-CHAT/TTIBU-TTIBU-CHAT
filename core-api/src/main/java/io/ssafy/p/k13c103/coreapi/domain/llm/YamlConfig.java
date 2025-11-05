0f8323dd5cf839325884ed7e6b92614fbaa9a3da
ahyolo27 <ahhyeon27@gmail.com>
2025-11-05 16:29:07 +0900

    feat: yaml로부터 카탈로그 읽어오는 로직 구현

---
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/llm/YamlConfig.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.k13c103.coreapi.domain.llm;
+
+import java.util.List;
+
+public record YamlConfig(
+        /* YAML 키와 동일 */
+        List<YamlModel> model_list,
+        GeneralSettings general_settings
+) {
+    public record YamlModel(String model_name, LiteParams litellm_params) {}
+    public record LiteParams(String model) {}
+    public record GeneralSettings(String master_key) {}
+}
