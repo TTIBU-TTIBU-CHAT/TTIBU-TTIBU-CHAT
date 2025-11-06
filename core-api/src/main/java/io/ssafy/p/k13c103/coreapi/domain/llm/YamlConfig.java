0e4e728f39fad717af702b7a69517edcdfa95281
ahyolo27 <ahhyeon27@gmail.com>
2025-11-06 10:17:02 +0900

    feat: YAML 파싱 시 지정하지 않은 키는 읽지 않도록 어노테이션 추가

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/llm/YamlConfig.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/llm/YamlConfig.java
@@ -1,7 +1,10 @@
 package io.ssafy.p.k13c103.coreapi.domain.llm;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record YamlConfig(
         /* YAML 키와 동일 */
         List<YamlModel> model_list,
