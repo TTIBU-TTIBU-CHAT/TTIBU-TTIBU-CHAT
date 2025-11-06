7caafd705ec5b06d836895cd23dfa4474e232d7f
ahyolo27 <ahhyeon27@gmail.com>
2025-11-06 10:24:20 +0900

    feat: provider_catalog 레포지토리 수정

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/catalog/repository/ProviderCatalogRepository.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/catalog/repository/ProviderCatalogRepository.java
@@ -7,13 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 public interface ProviderCatalogRepository extends JpaRepository<ProviderCatalog, Long>, ProviderCatalogRepositoryCustom  {
     List<ProviderCatalog> findByIsActiveTrueOrderByCodeAsc();
 
     boolean existsByCodeAndIsActiveTrue(String code);
 
+    /**
+     * 현재 전달된 code 목록에 포함되지 않는 모든 제공사를 soft delete 처리
+     */
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query(value = """
         UPDATE provider_catalog
