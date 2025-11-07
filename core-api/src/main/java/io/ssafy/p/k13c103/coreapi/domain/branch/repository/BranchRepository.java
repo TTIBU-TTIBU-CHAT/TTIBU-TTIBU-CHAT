c237ea8aa87aa19bda022db645ad64625b71eaa8
IM-GYURI <kgulr0517@gmail.com>
2025-11-07 15:37:54 +0900

    chore: 불필요한 클래스 및 코드 정리

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/branch/repository/BranchRepository.java
+++
@@ -1,8 +0,0 @@
-package io.ssafy.p.k13c103.coreapi.domain.branch.repository;
-
-import io.ssafy.p.k13c103.coreapi.domain.branch.entity.Branch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BranchRepository extends JpaRepository<Branch, Long> {
-
-}
--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/chat/service/AiSummaryService.java
+++
@@ -1,8 +0,0 @@
-package io.ssafy.p.k13c103.coreapi.domain.chat.service;
-
-import io.ssafy.p.k13c103.coreapi.domain.chat.dto.AiSummaryKeywordResponseDto;
-
-public interface AiSummaryService {
-
-    AiSummaryKeywordResponseDto generateSummaryAndKeywords(String text);
-}
