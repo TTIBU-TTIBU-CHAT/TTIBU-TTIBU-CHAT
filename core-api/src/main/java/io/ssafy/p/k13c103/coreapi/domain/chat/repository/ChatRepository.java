67f83557b739f4bdb01a4959187f58ce6de25b27
IM-GYURI <kgulr0517@gmail.com>
2025-11-07 15:31:11 +0900

    feat(chat): 그룹 내 채팅 조회 메서드 추가
    - findAllByGroup_GroupUid(Long groupUid) 메서드 추가
    - 특정 그룹에 속한 모든 Chat 리스트 조회 가능

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/chat/repository/ChatRepository.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/chat/repository/ChatRepository.java
@@ -1,16 +1,12 @@
 package io.ssafy.p.k13c103.coreapi.domain.chat.repository;
 
-import io.ssafy.p.k13c103.coreapi.domain.branch.entity.Branch;
 import io.ssafy.p.k13c103.coreapi.domain.chat.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-    @Query("SELECT c FROM Chat c WHERE c.branch = :branch ORDER BY c.createdAt DESC")
-    List<Chat> findTop5ByBranchOrderByCreatedAtDesc(@Param("branch") Branch branch);
+    List<Chat> findAllByGroup_GroupUid(Long groupUid);
 
 }
