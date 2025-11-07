044ecdaced6c6843faaed01ea83491eee07f0642
ahyolo27 <ahhyeon27@gmail.com>
2025-11-07 16:22:52 +0900

    feat: 사용 모델 다중 선택 요청 DTO 생성

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/model/dto/ModelRequestDto.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/model/dto/ModelRequestDto.java
@@ -2,4 +2,12 @@ package io.ssafy.p.k13c103.coreapi.domain.model.dto;
 
 public class ModelRequestDto {
 
+    /**
+     * 사용 모델 다중 선택 요청 DTO
+     */
+    public record SelectModel(
+            Long modelCatalogUid,
+            boolean isSelected // 사용자의 선택 여부
+    ) {
+    }
 }
