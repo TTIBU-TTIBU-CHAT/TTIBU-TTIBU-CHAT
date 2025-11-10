a44b2f041fbad56fe55cdfc3701168747dd9752e
DESKTOP-HA97GMI\SSAFY <hanjiu716@gmail.com>
2025-11-10 13:21:43 +0900

    refactor: 경로 파라미터로 수정

--- frontend/src/services/aiKeyService.js
+++ frontend/src/services/aiKeyService.js
@@ -4,6 +4,6 @@ export const aiKeyService = {
   getProviders: () => api.get('/models/providers'),
   createKey: (payload) => api.post('/keys', payload),
   updateKey: (payload) => api.put('/keys', payload),
-  getKey: (keyUid) => api.get(`/keys`, { params: { keyUid } }),
-  deleteKey: (keyUid) => api.delete(`/keys`, { params: { keyUid } }),
+  getKey: (keyUid) => api.get(`/keys/${keyUid}`),
+  deleteKey: (keyUid) => api.delete(`/keys/${keyUid}`),
 }
