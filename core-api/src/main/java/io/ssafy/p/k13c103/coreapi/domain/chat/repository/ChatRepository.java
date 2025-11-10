364a84222e1968942db3f238811d2ec43e40c632
IM-GYURI <kgulr0517@gmail.com>
2025-11-10 17:39:52 +0900

    feat(chat): 그룹 복제 노드 전용 삭제 메서드 deleteAllGroupCopies 추가

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/chat/repository/ChatRepository.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/chat/repository/ChatRepository.java
@@ -2,6 +2,9 @@ package io.ssafy.p.k13c103.coreapi.domain.chat.repository;
 
 import io.ssafy.p.k13c103.coreapi.domain.chat.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,12 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
     List<Chat> findAllByGroup_GroupUid(Long groupUid);
 
     void deleteAllByGroup_GroupUid(Long groupUid);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+    delete from Chat c
+    where c.group.groupUid = :groupId
+      and c.chatType = io.ssafy.p.k13c103.coreapi.domain.chat.enums.ChatType.GROUP
+    """)
+    int deleteAllGroupCopies(@Param("groupId") Long groupId);
 }
