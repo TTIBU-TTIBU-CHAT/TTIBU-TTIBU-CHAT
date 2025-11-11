77c19a53608c0125aa86aef9590d1aaa5f0748cf
IM-GYURI <kgulr0517@gmail.com>
2025-11-11 15:34:23 +0900

    feat(room): 기존 Room에 새 채팅 노드 생성 로직 구현 (QUESTION_CREATED SSE 이벤트 포함)

--- core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/room/service/RoomService.java
+++ core-api/src/main/java/io/ssafy/p/k13c103/coreapi/domain/room/service/RoomService.java
@@ -1,5 +1,7 @@
 package io.ssafy.p.k13c103.coreapi.domain.room.service;
 
+import io.ssafy.p.k13c103.coreapi.domain.chat.dto.ChatCreateRequestDto;
+import io.ssafy.p.k13c103.coreapi.domain.chat.dto.ChatCreateResponseDto;
 import io.ssafy.p.k13c103.coreapi.domain.room.dto.RoomCreateRequestDto;
 import io.ssafy.p.k13c103.coreapi.domain.room.dto.RoomRenameRequestDto;
 import io.ssafy.p.k13c103.coreapi.domain.room.dto.RoomRenameResponseDto;
@@ -11,4 +13,6 @@ public interface RoomService {
     void isOwner(Long memberId, Long roomId);
 
     RoomRenameResponseDto updateRoomName(Long memberId, Long roomId, RoomRenameRequestDto request);
+
+    ChatCreateResponseDto createChatInRoom(Long memberId, Long roomId, ChatCreateRequestDto request);
 }
